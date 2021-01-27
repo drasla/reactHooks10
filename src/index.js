804fe6ee4feddec083206010418929ba6b66f4f5
Tae.Garam <drasla@naver.com>
2021-01-27 23:56:42 +0900

    #1.1 useInput

--- src/index.js
+++ src/index.js
@@ -3,50 +3,24 @@ import ReactDOM from 'react-dom';
 
 import "./styles.css";
 
-const App = () => {
-    const [item, setItem] = useState(1);
-    const incrementItem = () => setItem(item + 1);
-    const decrementItem = () => setItem(item - 1);
+const useInput = (initialValue) => {
+    const [value, setValue] = useState(initialValue);
+    const onChange = (event) => {
 
+    }
+    return {value, onChange};
+}
+
+const App = () => {
+    const name = useInput("Mr.");
     return (
         <div className="App">
-            <h1>Hello {item}</h1>
-            <button onClick={incrementItem}>Increment</button>
-            <button onClick={decrementItem}>Decrement</button>
+            <h1>Hello</h1>
+            <input placeholder="Name" {...name}/>
         </div>
     );
 }
 
-class AppUgly extends React.Component {
-    state = {
-        item: 1
-    }
-
-    render() {
-        const {item} = this.state;
-        return (
-            <div className="App">
-                <h1>Hello {item}</h1>
-                <button onClick={this.incrementItem}>Increment</button>
-                <button onClick={this.decrementItem}>Decrement</button>
-            </div>
-        );
-    }
-    incrementItem = () => {
-        this.setState(state => {
-            return {
-                item: state.item + 1
-            };
-        });
-    };
-    decrementItem = () => {
-        this.setState(state => {
-            return {
-                item: state.item -1
-            }
-        });
-    }
-}
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<AppUgly />, rootElement);
+ReactDOM.render(<App />, rootElement);
