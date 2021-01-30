8d54eb5d1bc57a166a34de22731b6a5733b2f5fa
Tae.Garam <drasla@naver.com>
2021-01-30 22:58:12 +0900

    #2.1 useTitle (Modify)

--- src/index.js
+++ src/index.js
@@ -1,20 +1,11 @@
 import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import "./styles.css";
-
-const useTitle = (initialTitle) => {
-    const [title, setTitle] = useState(initialTitle);
-    const updateTitle = () => {
-        const htmlTitle = document.querySelector("title");
-        htmlTitle.innerText = title;
-    };
-    useEffect(updateTitle, [title]);
-    return setTitle;
-}
+import {useTitle} from "./useTitle";
 
 const App = () => {
     const titleUpdater = useTitle("Loading");
-    setTimeout(() => titleUpdater("Home", 5000));
+    setTimeout(() => titleUpdater("Home"), 5000);
     return (
         <div className="App">
             <div>Hi</div>
