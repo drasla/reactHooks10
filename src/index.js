2d8632898b9487a43bf7cbe668d3e84c076fc29a
Tae.Garam <drasla@naver.com>
2021-02-11 17:39:05 +0900

    #2.7 useNotification

--- src/index.js
+++ src/index.js
@@ -1,24 +1,14 @@
 import React, {useState, useEffect, useRef} from 'react';
 import ReactDOM from 'react-dom';
 import "./styles.css";
-import {useScroll} from "./useScroll";
-import {useFullscreen} from "./useFullscreen";
+import {useNotification} from "./useNotification";
 
 const App = () => {
-    const {y} = useScroll();
-    const onFullS = isFull => {
-        console.log(isFull ? "We are full" : "We are small");
-    }
-    const {element, triggerFull, exitFull} = useFullscreen(onFullS);
-
+    const trigerNotif = useNotification("Can I steal your kimchi?", {body: "I Love kimchi you"});
     return (
         <div className="App" style={{ height: "1000vh" }}>
-            <h1 style={{ position: "fixed", color: y > 100 ? "red" : "blue"}}>Hi</h1>
-            <div ref={element}>
-                <img alt="" src="https://s.pstatic.net/static/www/mobile/edit/2021/0209/cropImg_728x360_54798928890668437.jpeg" />
-                <button onClick={exitFull}>Exit fullscreen</button>
-            </div>
-            <button onClick={triggerFull}>Make fullscreen</button>
+            <h1>Hi</h1>
+            <button onClick={trigerNotif}>Hello</button>
         </div>
     );
 }
