3fa2678f0a40daf4068223eacbfdeba33b00bfbc
Tae.Garam <drasla@naver.com>
2021-02-11 19:09:23 +0900

    #2.8 useAxios

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React, {useState, useEffect, useRef} from 'react';
 import ReactDOM from 'react-dom';
 import "./styles.css";
-import {useNotification} from "./useNotification";
+import useAxios from "./useAxios";
 
 const App = () => {
-    const trigerNotif = useNotification("Can I steal your kimchi?", {body: "I Love kimchi you"});
+    const {loading, error, data, refetch} = useAxios({url: "https://api.coinpaprika.com/v1/coins"});
+    console.log(`${loading} , ${error} , ${JSON.stringify(data)}`)
     return (
-        <div className="App" style={{ height: "1000vh" }}>
-            <h1>Hi</h1>
-            <button onClick={trigerNotif}>Hello</button>
+        <div className="App">
+            <h1>{data && data.status}</h1>
+            <h2>{loading && "Loading"}</h2>
+            <button onClick={refetch}>Refetch</button>
         </div>
     );
 }
